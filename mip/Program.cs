759694273bc5b8c7f4c405efa35ee918b5579345
faults904 <faults904@gmail.com>
2015-12-03 00:28:02 +0900

    gogo!

--- mip/Program.cs
+++ mip/Program.cs
@@ -16,7 +16,7 @@ namespace mip
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MIP());
         }
     }
 }

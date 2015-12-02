b6fdc36ed4ce30374da418a10bef17ab21305083
faults904 <faults904@gmail.com>
2015-12-02 23:29:57 +0900

    화면 구성 중

--- mip/MIP.cs
+++ mip/MIP.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace mip
 {
-    public partial class Form1 : Form
+    public partial class MIP : Form
     {
-        public Form1()
+        public MIP()
         {
             InitializeComponent();
         }
